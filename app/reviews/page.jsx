8b3b7908d3b2fc8be7a0eb97cdad222b56eb4ac4
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-08-19 19:28:17 -0400

    Add review links

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 export default function ReviewsPage() {
   return (
     <>
       <h1>Reviews</h1>
+      <ul>
+        <li>
+          <Link href="/reviews/hollow-knight">
+            Hollow Knight
+          </Link>
+          <Link href="/reviews/stardew-valley">
+            Stardew Valley
+          </Link>
+        </li>
+      </ul>
       <p>
         Here we'll list all the reviews.
       </p>
