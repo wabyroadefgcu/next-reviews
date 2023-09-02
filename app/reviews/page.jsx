1746b6347147abea045ec5a7d00a79d770d0ad55
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-09-02 19:10:04 -0400

    Up to Dynamic Metadata

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,31 +1,31 @@
 import Link from 'next/Link';
 import Heading from '@/components/Heading';
+import { getReviews } from '@/lib/reviews';
 
-export default function ReviewsPage() {
+export const metadata = {
+  title: 'Reviews',
+};
+
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
+
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img src="/images/hollow-knight.jpg" alt=""
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img src="/images/stardew-valley.jpg" alt=""
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li key={review.slug}
+          className="bg-white border rounded shadow w-80 hover:shadow-xl">
+            <Link href={`/reviews/${review.slug}`}>
+              <img src={review.image} alt=""
+                width="320" height="180" className="rounded-t"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
---
+++ content/reviews/hellblade.md
@@ -0,0 +1,9 @@
+---
+title: "Hellblade"
+date: "2023-05-06"
+image: "/images/hellblade.jpg"
+---
+
+__Hellblade__ is a a whole other game.
+
+It's okay.
