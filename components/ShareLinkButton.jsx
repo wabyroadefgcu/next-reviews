202b9970dae14427489fb1197a4fa8bfb8999e7f
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-09-02 20:11:28 -0400

    ShareLinkButton icon

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,14 +1,24 @@
 'use client';
+
+import { LinkIcon } from '@heroicons/react/20/solid'
+import { useState } from "react";
+
 export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
-    console.log('clicked');
+    navigator.clipboard.writeText(window.location.href);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 1500);
   };
 
+  console.log('[ShareLinkButton] clicked:', clicked);
   return (
     <button onClick={handleClick}
-      className="border px-2 rounded text-slate-500 text-sm
+      className="border flex gap-1 items-center px-2 rounded text-slate-500 text-sm
                hover:bg-orange-100 hover:text-slate-700">
-      Share Link
+      <LinkIcon className="h-4 w-4" />
+      {clicked ? 'Link copied!' : 'Share link'}
     </button>
   );
 }
