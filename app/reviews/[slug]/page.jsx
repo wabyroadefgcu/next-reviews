100e0d0bdf2e146e43ebcca2c44eb413c9c7deaa
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-09-07 06:56:00 -0400

    a bit busted

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -2,10 +2,10 @@ import Heading from '@/components/Heading';
 import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
 
-export async function generateStaticParams() {
-  const slugs = await getSlugs();
-  return slugs.map((slug) => ({ slug }));
-}
+// export async function generateStaticParams() {
+//   const slugs = await getSlugs();
+//   return slugs.map((slug) => ({ slug }));
+// }
 
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug);
@@ -16,7 +16,7 @@ export async function generateMetadata({ params: { slug } }) {
 
 export default async function ReviewPage({ params: { slug } }) {
   const review = await getReview(slug);
-  console.log('[ReviewPage] rendering', slug);
+  console.log('[ReviewPage] rendering', review);
   return (
     <>
       <Heading>{review.title}</Heading>
--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,15 +1,30 @@
 import { writeFileSync } from 'node:fs';
 import qs from 'qs';
 
+// const url = 'http://localhost:1337/api/reviews' + '?' + qs.stringify({
+//     fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+//     populate: { image: { fields: ['url'] } },
+//     sort: ['publishedAt:desc'],
+//     pagination: { pageSize: 6 },
+//   }, { encodeValuesOnly: true });
+// console.log('url:', url);
+// const response = await fetch(url);
+// const body = await response.json();
+// const formatted = JSON.stringify(body, null, 2);
+// const file = 'scripts/strapi-response.json';
+// writeFileSync(file, formatted, 'utf8');
+
+
 const url = 'http://localhost:1337/api/reviews' + '?' + qs.stringify({
-    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    filters: { slug: { $eq: 'hades-2018' } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
     populate: { image: { fields: ['url'] } },
-    sort: ['publishedAt:desc'],
-    pagination: { pageSize: 6 },
+    pagination: { pageSize: 1, withCount: false },
   }, { encodeValuesOnly: true });
 console.log('url:', url);
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
-const file = 'scripts/strapi-response.json';
-writeFileSync(file, formatted, 'utf8');
+console.log(formatted);
+// const file = 'scripts/strapi-response.json';
+// writeFileSync(file, formatted, 'utf8');
