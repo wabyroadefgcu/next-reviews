dfe3c23637cdea0b89a4c7e818d3a91562f7824e
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-09-18 16:20:10 -0400

    Optimized images, subtitles

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import Link from 'next/link';
 import Heading from '@/components/Heading';
-import { getFeaturedReview } from '@/lib/reviews';
+import Image from 'next/image';
+import Link from 'next/link';
+import { getReviews } from '@/lib/reviews';
 
 export default async function HomePage() {
-  const review = await getFeaturedReview();
+  const reviews = await getReviews(4);
+
   console.log('[HomePage] rendering');
   return (
     <>
@@ -11,19 +13,29 @@ export default async function HomePage() {
       <p className="pb-3">
         Only the best indie games, reviewed for you.
       </p>
-      <div className="bg-white border rounded shadow w-80
-                      hover:shadow-xl sm:w-full">
-        <Link href={`/reviews/${review.slug}`}
-          className="flex flex-col sm:flex-row">
-          <img src={review.image} alt=""
-            width="320" height="180"
-            className="rounded-t sm:rounded-l sm:rounded-r-none"
-          />
-          <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
-            {review.title}
-          </h2>
-        </Link>
-      </div>
+      <ul className="flex flex-col gap-3">
+        {reviews.map((review, index) => (
+          <li key={review.slug}
+            className="bg-white border rounded shadow w-80
+                        hover:shadow-xl sm:w-full">
+            <Link href={`/reviews/${review.slug}`}
+              className="flex flex-col sm:flex-row">
+              <Image src={review.image} alt="" priority={index << 2}
+                width="320" height="180"
+                className="rounded-t sm:rounded-l sm:rounded-r-none"
+              />
+              <div className="px-2 py-1 text-center sm:text-left">
+                <h2 className="font-orbitron font-semibold">
+                  {review.title}
+                </h2>
+                <p className="hidden pt-2 sm:block">
+                  {review.subtitle}
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))};
+      </ul>
     </>
   );
 }
--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
 import Heading from '@/components/Heading';
+import Image from 'next/image';
+import Link from 'next/link';
 import { getReviews } from '@/lib/reviews';
 
 export const metadata = {
@@ -7,18 +8,18 @@ export const metadata = {
 };
 
 export default async function ReviewsPage() {
-  const reviews = await getReviews();
+  const reviews = await getReviews(6);
   console.log('[ReviewsPage] reviews:', reviews);
 
   return (
     <>
       <Heading>Reviews</Heading>
       <ul className="flex flex-row flex-wrap gap-3">
-        {reviews.map((review) => (
+        {reviews.map((review, index) => (
           <li key={review.slug}
           className="bg-white border rounded shadow w-80 hover:shadow-xl">
             <Link href={`/reviews/${review.slug}`}>
-              <img src={review.image} alt=""
+              <Image src={review.image} alt="" priority={index << 2}
                 width="320" height="180" className="rounded-t"
               />
               <h2 className="font-orbitron font-semibold py-1 text-center">
