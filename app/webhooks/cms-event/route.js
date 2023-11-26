f80969c204debe54c7cc5427df571ac10b82e007
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2023-11-25 20:11:15 -0500

    auto revalidation

---
+++ app/webhooks/cms-event/route.js
@@ -0,0 +1,12 @@
+import { revalidateTag } from 'next/cache';
+import { NextResponse } from 'next/server';
+import { CACHE_TAG_REVIEWS } from '@/lib/reviews';
+
+export async function POST(request) {
+  const payload = await request.json();
+  if (payload.model === 'review') {
+    revalidateTag(CACHE_TAG_REVIEWS);
+    console.log('revalidated: ', CACHE_TAG_REVIEWS);
+  }
+  return new Response(null, { status: 204 });
+}
